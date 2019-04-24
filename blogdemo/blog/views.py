52d13890cb2412e0d292503611a9a3cbf7dea75f
dh <2964727542@qq.con>
2019-04-24 15:58:22 +0800

    实现了主页的分页设置，创建了模板的父模板

--- blogdemo/blog/views.py
+++ blogdemo/blog/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render, reverse, redirect
 from .models import Article, Author, Sort, Labels, Comment
-from django.http import HttpRequest,HttpResponse,HttpResponseRedirect
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
 # Create your views here.
+# 主页
 def index(request):
     articles = Article.objects.all()
     articless = Article.objects.all()[:3]
     sorts = Sort.objects.all()
     labelss = Labels.objects.all()
+    # 归档显示所需的截止到月份的时间对象集
     dates = Article.objects.datetimes('create_time', 'month', order='DESC')
-    # print(dates)
+    # 分页用到的代码
+    paginator = Paginator(articles, 4, 2)
+    page = request.GET.get('page')
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
     return render(request, 'blog/index.html', {'articles':articles, 'articless':articless, 'sorts':sorts,
                                                'labelss':labelss, 'dates':dates})
 
 
+# 详情页
 def single(request, id):
     if request.method == 'GET':
         article = Article.objects.get(pk=id)
@@ -34,4 +45,10 @@ def single(request, id):
         comment.save()
         # return HttpResponseRedirect('/blog/single/{}'.format(id))
         return redirect(reverse('blog:single', args=(id,)))
+
+
+# 归档
+def archive(request, month, year):
+    pass
+
 
